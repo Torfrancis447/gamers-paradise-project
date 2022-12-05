4ef58abceb7b97df74718de16763c42e47fd72bc
Javier Rodriguez <javi.rodriguez.martinz@gmail.com>
2022-12-05 12:22:14 -0600

    gamelist add

--- gamers-paradise-project/src/App.js
+++ gamers-paradise-project/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import GameList from './components/GameList';
+import Header from './Header';
+import Search from './Search'
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+      <Search/>
+      <GameList/>
     </div>
   );
 }

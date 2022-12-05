129184823046b668ad1c9066757aa212de55787e
Javier Rodriguez <javi.rodriguez.martinz@gmail.com>
2022-12-05 12:09:37 -0600

    add card

--- gamers-paradise-project/src/App.js
+++ gamers-paradise-project/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import GameList from './components/GameList';
 
 function App() {
   return (
     <div>
-      
+      <GameList />
     </div>
   );
 }
---
+++ gamers-paradise-project/src/components/GameList.js
@@ -0,0 +1,10 @@
+import React from "react"
+import GameCard from "./GameCard"
+
+
+function GameList() {
+    return (
+        <GameCard/>
+    )
+}
+export default GameList
